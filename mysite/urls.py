871aa94b06655b7f638c4293bb6751e2c133610b
Kaushalendra Mahato <kaushalendramahato87@gmail.com>
2026-06-28 11:37:07 +0545

    Built the users app for the login feature

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
     path('cart/', include('cart.urls')),
+    path('users/',include('users.urls')),
+    
 ]
 
 urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
